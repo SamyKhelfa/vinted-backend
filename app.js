6884183cc4444c8592fd5c22aed882e6fc7a1b30
SamyKhelfa <60275651+SamyKhelfa@users.noreply.github.com>
2023-06-01 17:43:03 +0200

    update

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ const app = express();
 const mongoose = require("mongoose");
 
 // Connexion à MongoDB
-mongoose.connect("mongodb://127.0.0.1/Vinted", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://127.0.0.1/Vinted");
 
 // Importer les fichiers de routes
 const userRoutes = require("./routes/userRoutes");
 const offerRoutes = require("./routes/offerRoutes");
 
 app.use(express.json());
-
 // Utiliser les routes
 app.use("/user", userRoutes);
 app.use("/offer", offerRoutes);
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "This route does not exist" });
+  console.log();
+});
 
 app.listen(3000, () => {
   console.log("Server started");
