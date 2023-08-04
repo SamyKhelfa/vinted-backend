a7aa467e648d495f0fa63587992fdcc851512e89
SamyKhelfa <60275651+SamyKhelfa@users.noreply.github.com>
2023-08-04 18:06:23 +0200

    update

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ router.all("*", (req, res) => {
   return res.status(404).json("Not found");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started");
 });
